fc53af7ab574f820007de693e96a13b290bf279d
raygervais <raygervais@hotmail.ca>
2020-04-30 19:27:12 -0400

    adds: gin-powered hello world

---
+++ src/backend/main_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func performRequest(r http.Handler, method, path string) *httptest.ResponseRecorder {
+	req, _ := http.NewRequest(method, path, nil)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	return w
+}
+
+func TestHelloWorld(t *testing.T) {
+	// Build our expected body
+	body := gin.H{
+		"message": "Hello, World!",
+	}
+
+	// Grab our router
+	router := SetupRouter()                 // Perform a GET request with that handler.
+	w := performRequest(router, "GET", "/") // Assert we encoded correctly,
+
+	// the request gives a 200
+	assert.Equal(t, http.StatusOK, w.Code) // Convert the JSON response to a map
+
+	var response map[string]string
+	err := json.Unmarshal([]byte(w.Body.String()), &response) // Grab the value & whether or not it exists
+
+	value, exists := response["message"] // Make some assertions on the correctness of the response.
+
+	assert.Nil(t, err)
+	assert.True(t, exists)
+	assert.Equal(t, body["message"], value)
+}
