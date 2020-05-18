e7088e1396653e229f76c0f123ec828384add09c
Grommers00 <james.inkster@live.ca>
2020-05-17 23:51:42 -0400

    adds #20 semantic-ui to react application.

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 
+// Example of how to import semantic-ui into react project.
+import './semantic/dist/semantic.css'
+import { Button } from 'semantic-ui-react';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {/* This button is to show the implementation of semantic-UI for now.*/}
+        <Button>Setting up Semantic-UI</Button>
         <p>Hello World!</p>
       </header>
     </div>
