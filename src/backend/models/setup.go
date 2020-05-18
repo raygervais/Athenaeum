0acb1ff868b0920966fe7e781b81be19ea75bfad
raygervais <raygervais@hotmail.ca>
2020-05-18 00:14:00 -0400

    fixes: missing comments

--- src/backend/models/setup.go
+++ src/backend/models/setup.go
@@ -2,9 +2,10 @@ package models
 
 import (
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	_ "github.com/jinzhu/gorm/dialects/sqlite" //SQLite Driver
 )
 
+// SetupModels handles creation of DB Tables
 func SetupModels(dbTarget string) *gorm.DB {
 	db, err := gorm.Open("sqlite3", dbTarget)
 
