abe19592eff2fac8dea9cf118182bb47b72b09bf
James Inkster <42331013+Grommers00@users.noreply.github.com>
2020-06-07 08:19:27 -0400

    add #20 - Add Semantic UI to Front-end (#36)

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+
+import 'fomantic-ui-css/semantic.css';
+import { Container, Header } from 'semantic-ui-react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Container>
+          <Header className="yellow">
+            Athenaeum
+          </Header>
+        </Container>
+
         <p>Hello World!</p>
       </header>
     </div>
